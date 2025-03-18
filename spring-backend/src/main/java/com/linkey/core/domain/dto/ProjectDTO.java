6a0fd5da1f8efdf866db0fe19c7a5a6fd9e2fc3e
dyoun12 <gdaeyeon002@gmail.com>
2025-03-19 03:35:56 +0900

    feat : 엔티티 반환용 ProjectDTO 생성
    Signed-off-by: dyoun12 <gdaeyeon002@gmail.com>

---
+++ spring-backend/src/main/java/com/linkey/core/domain/dto/ProjectDTO.java
@@ -0,0 +1,35 @@
+package com.linkey.core.domain.dto;
+
+import com.linkey.core.domain.entity.Project;
+import com.linkey.core.domain.entity.Team;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@ToString
+public class ProjectDTO{
+    private Integer projectId;
+    private String projectName;
+    private String projectDesc;
+    private Team team; // FK (team_members.team_id)
+    private String githubRepoUrl;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    public static ProjectDTO toDTO(Project project) {
+        return ProjectDTO.builder()
+                .projectId(project.getProjectId())
+                .projectName(project.getProjectName())
+                .projectDesc(project.getProjectDesc())
+                .team(project.getTeam())
+                .githubRepoUrl(project.getGithubRepoUrl())
+                .createdAt(project.getCreatedAt())
+                .updatedAt(project.getUpdatedAt())
+                .build();
+    }
+}
