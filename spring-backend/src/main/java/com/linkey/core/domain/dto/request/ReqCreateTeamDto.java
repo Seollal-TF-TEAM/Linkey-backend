5d9994e5a5fb1689d4b15062e4e7813d1ba9cb0c
dyoun12 <gdaeyeon002@gmail.com>
2025-03-23 20:21:12 +0900

    feat : teams 테이블에 관한 DTO, API 명세서 기준 적용

---
+++ spring-backend/src/main/java/com/linkey/core/domain/dto/request/ReqCreateTeamDto.java
@@ -0,0 +1,35 @@
+package com.linkey.core.domain.dto.request;
+
+import lombok.*;
+
+import java.util.List;
+
+@Getter
+@Setter
+@Builder
+@ToString
+@AllArgsConstructor
+public class ReqCreateTeamDto {
+    private String teamName;
+    private String teamDesc;
+    private List<SingleTeamMember> teamMembers;
+
+    @Builder
+    @ToString
+    @AllArgsConstructor
+    public static class SingleTeamMember {
+        long githubUserId;
+    }
+}
+/*
+예시 :
+    ReqCreateTeamDto.builder()
+                .teamName("name")
+                .teamDesc("desc")
+                .teamMembers(List.of(
+                        ReqCreateTeamDto.SingleTeamMember.builder()
+                                .githubUserId(123L)
+                                .build()
+                    ))
+                .build();
+ */
