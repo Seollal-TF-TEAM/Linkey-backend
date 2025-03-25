eeddb11a47d198f89082630498cee8e0f5a2bac9
dyoun12 <gdaeyeon002@gmail.com>
2025-03-25 16:05:30 +0900

    feat : updateProject 메소드 추가

--- spring-backend/src/main/java/com/linkey/core/controller/ProjectController.java
+++ spring-backend/src/main/java/com/linkey/core/controller/ProjectController.java
@@ -1,35 +1,84 @@
 package com.linkey.core.controller;
 
+import com.linkey.core.domain.dto.request.ReqUpdateProjectDto;
+import com.linkey.core.domain.dto.response.ErrorResult;
+import com.linkey.core.domain.dto.response.ResWrapper;
+import com.linkey.core.domain.dto.response.SuccessResult;
 import com.linkey.core.domain.entity.Project;
+import com.linkey.core.exception.CustomException;
+import com.linkey.core.exception.ErrorCode;
+import com.linkey.core.service.project.ProjectService;
+import com.linkey.core.service.project.ProjectServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Objects;
 
 @Controller
 @RequestMapping("/api/projects")
 public class ProjectController {
 
-    @GetMapping("getPageProjectList")
+    private final ProjectService projectService;
+
+    public ProjectController (ProjectService projectService) {
+        this.projectService = projectService;
+    }
+
+    @GetMapping("projectList")
     public List<Project> getPageProjectList() {
         return null;
     }
 
-    @PostMapping("create")
-    public Boolean createProject() {
-        return null;
-    }
-
-    @GetMapping("getPageProjectDetail")
+    @GetMapping("projectDetail")
     public Project getPageProjectDetail() {
         return null;
     }
 
-    @PatchMapping("udpate")
-    public Boolean udpateProject() {
+    @PostMapping("createProject")
+    public Boolean createProject() {
+        return null;
+    }
+
+    @PatchMapping("updateProject")
+    public ResWrapper updateProject(@RequestBody ReqUpdateProjectDto reqUpdateProjectDto) {
+        ResWrapper result = null;
+        try {
+            Integer projectId = projectService.updateProject(reqUpdateProjectDto);
+            result = ResWrapper.builder()
+                    .result(
+                            SuccessResult.builder()
+                                    .status("Success")
+                                    .data(projectId)
+                                    .build()
+                    ).build();
+            return result;
+        } catch (CustomException e) {
+            result = ResWrapper.builder()
+                    .result(
+                            ErrorResult.builder()
+                                    .status(e.getErrorCode().getHttpStatus().value())
+                                    .errorCode(e.getErrorCode().getCode())
+                                    .message(e.getErrorCode().getMessage())
+                    ).build();
+            return result;
+        } catch (Exception e) {
+            result = ResWrapper.builder()
+                    .result(
+                            ErrorResult.builder()
+                                    .status(400)
+                                    .errorCode(9999)
+                                    .message(e.getMessage())
+                                    .build()
+                    ).build();
+            return result;
+        }
+
+    }
+
+    @PatchMapping("deleteProject")
+    public Boolean deleteProject() {
         return null;
     }
 }
--- spring-backend/src/main/java/com/linkey/core/exception/ErrorCode.java
+++ spring-backend/src/main/java/com/linkey/core/exception/ErrorCode.java
@@ -9,10 +9,21 @@ public enum ErrorCode {
 
     // 2000번대: 팀 관련 예외 처리
     TEAM_NOT_FOUND(2001, "해당 팀을 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
-    DUPLICATE_TEAM_MEMBER(2002, "이미 존재하는 팀원입니다.", HttpStatus.BAD_REQUEST);
+    DUPLICATE_TEAM_MEMBER(2002, "이미 존재하는 팀원입니다.", HttpStatus.BAD_REQUEST),
+
 
     // 3000번대 : 프로젝트 관련 예외처리
+    // 3001~3099 : DQL 관련 예외
+    PROJECT_NOT_FOUND(3001, "해당 프로젝트를 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
 
+    // 3100~3399 : DML 관련 예외처리
+    DUPLICATE_PROJECT(3102, "이미 존재하는 프로젝트입니다..", HttpStatus.BAD_REQUEST),
+
+
+    // 3400~3999 : 기타 예외처리
+
+
+    ;
 
     private final int code;
     private final String message;
