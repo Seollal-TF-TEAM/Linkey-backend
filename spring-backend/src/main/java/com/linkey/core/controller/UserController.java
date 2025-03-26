96045bf817cf0795cc89bfc0e69cbf3b6387d7a3
HanJH <71498489+letsgojh0810@users.noreply.github.com>
2025-03-26 18:21:59 +0900

    feat : delete origin url

--- spring-backend/src/main/java/com/linkey/core/controller/UserController.java
+++ spring-backend/src/main/java/com/linkey/core/controller/UserController.java
@@ -14,7 +14,6 @@ import java.util.Map;
 @RestController
 @RequestMapping("/user")
 @RequiredArgsConstructor
-@CrossOrigin(origins = "http://localhost:3000", allowCredentials = "true")
 public class UserController {
 
     private final RedisTemplate<String, String> redisTemplate;
