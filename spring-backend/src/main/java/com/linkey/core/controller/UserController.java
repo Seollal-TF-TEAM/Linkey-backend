bc7a54c8c8c422d47ccfce5a0df80b2b2d8969c3
HanJH <71498489+letsgojh0810@users.noreply.github.com>
2025-03-26 18:20:58 +0900

    feat : update UserInfo

--- spring-backend/src/main/java/com/linkey/core/controller/UserController.java
+++ spring-backend/src/main/java/com/linkey/core/controller/UserController.java
@@ -1,11 +1,14 @@
 package com.linkey.core.controller;
 
+import com.linkey.core.domain.entity.GitUser;
+import com.linkey.core.repository.user.GitUserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @RestController
@@ -15,7 +18,22 @@ import java.util.Map;
 public class UserController {
 
     private final RedisTemplate<String, String> redisTemplate;
+    private final GitUserRepository gitUserRepository;
 
+    // user 정보 가져오기 (nav bar)
+    @GetMapping("")
+    public ResponseEntity<?> getUser(@RequestParam Long githubUserId) {
+        return gitUserRepository.findByGithubUserId(githubUserId)
+                .map(user -> {
+                    Map<String, Object> response = new HashMap<>();
+                    response.put("githubUserName", user.getGithubUserName());
+                    response.put("githubProfileUrl", user.getGithubProfileUrl());
+                    return ResponseEntity.ok(response);
+                })
+                .orElse(ResponseEntity.ok(null)); // orElseThrow 말고 null 리턴
+    }
+
+    // userId로 accessToken 직접 조회 (예: 테스트용)
     @GetMapping("/token")
     public ResponseEntity<?> getAccessToken(@RequestParam Long userId) {
         String token = redisTemplate.opsForValue().get("github:token:" + userId);
