8e485424ae666b3b58ed5aa92037ece62bf1165c
dyoun12 <gdaeyeon002@gmail.com>
2025-03-27 22:25:01 +0900

    refact : findProjectsByGithubUser 메소드 queryDSL 적용
    Signed-off-by: dyoun12 <gdaeyeon002@gmail.com>

--- spring-backend/src/main/java/com/linkey/core/repository/project/custom/ProjectRepositoryImpl.java
+++ spring-backend/src/main/java/com/linkey/core/repository/project/custom/ProjectRepositoryImpl.java
@@ -2,10 +2,10 @@ package com.linkey.core.repository.project.custom;
 
 import com.linkey.core.domain.dto.ProjectDto;
 import com.linkey.core.domain.entity.Project;
-//import com.linkey.core.domain.entity.QProject;
-//import com.linkey.core.domain.entity.QTeam;
-//import com.linkey.core.domain.entity.QTeamMember;
-//import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.linkey.core.domain.entity.QProject;
+import com.linkey.core.domain.entity.QTeam;
+import com.linkey.core.domain.entity.QTeamMember;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Repository;
@@ -17,32 +17,19 @@ import java.util.List;
 @RequiredArgsConstructor
 public class ProjectRepositoryImpl implements ProjectRepositoryCustom {
 
-//    private final JPAQueryFactory queryFactory;
+    private final JPAQueryFactory queryFactory;
 
-//    @Override
-//    public List<ProjectDto> findProectByGithubUserId(Long githubUserId) {
-//        return List.of();
-//    }
-//    @Override
-//    public List<Project> findByProjectId(Integer projectId) {
-//        return List.of();
-//    }
-//
     @Override
-    public List<Project> findProectsByGithubUserId(Long githubUserId) {
-//        QProject project = QProject.project;
-//        QTeam team = QTeam.team;
-//        QTeamMember teamMember = QTeamMember.teamMember;
-//
-//        List<Project> projects = queryFactory
-//                .selectFrom(project)
-//                .join(project.team, team)
-//                .join(team.teamMembers, teamMember)
-//                .where(teamMember.user.githubUserId.eq(githubUserId))
-//                .fetch();
-//
-//        return projects.stream().map(ProjectDto::fromEntity).toList();
-        return null;
-    }
+    public List<Project> findProjectsByGithubUserId(Long githubUserId) {
+        QProject project = QProject.project;
+        QTeam team = QTeam.team;
+        QTeamMember teamMember = QTeamMember.teamMember;
 
+        return queryFactory
+                .selectFrom(project)
+                .join(project.team, team).fetchJoin()
+                .join(team.teamMembers, teamMember).fetchJoin()
+                .where(teamMember.user.githubUserId.eq(githubUserId))
+                .fetch();
+    }
 }
