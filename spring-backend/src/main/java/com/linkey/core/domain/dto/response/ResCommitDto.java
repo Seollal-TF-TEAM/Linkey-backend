dcbd21f62518e8e5f3975ba6908bfa653345e8ea
HanJH <71498489+letsgojh0810@users.noreply.github.com>
2025-03-30 16:38:58 +0900

    feat : Add Commit Controller, dto

---
+++ spring-backend/src/main/java/com/linkey/core/domain/dto/response/ResCommitDto.java
@@ -0,0 +1,13 @@
+package com.linkey.core.domain.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+@AllArgsConstructor
+public class ResCommitDto {
+    private String message;  // 커밋 메시지
+    private String author;   // 작성자 이름
+}
