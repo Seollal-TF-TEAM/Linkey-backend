aeb37a0968a247bc08754b012a67b182e2144dba
dyoun12 <gdaeyeon002@gmail.com>
2025-04-01 22:16:09 +0900

    refact : 불필요한 import문 제거
    Signed-off-by: dyoun12 <gdaeyeon002@gmail.com>

--- spring-backend/src/main/java/com/linkey/core/repository/project/ProjectRepository.java
+++ spring-backend/src/main/java/com/linkey/core/repository/project/ProjectRepository.java
@@ -1,12 +1,8 @@
 package com.linkey.core.repository.project;
 
-import com.linkey.core.domain.dto.ProjectDto;
 import com.linkey.core.domain.entity.Project;
 import com.linkey.core.repository.project.custom.ProjectRepositoryCustom;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 public interface ProjectRepository extends JpaRepository<Project, Integer>, ProjectRepositoryCustom {
 
--- spring-backend/src/main/java/com/linkey/core/repository/project/custom/ProjectRepositoryCustom.java
+++ spring-backend/src/main/java/com/linkey/core/repository/project/custom/ProjectRepositoryCustom.java
@@ -1,7 +1,6 @@
 package com.linkey.core.repository.project.custom;
 
 import com.linkey.core.domain.entity.Project;
-import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
