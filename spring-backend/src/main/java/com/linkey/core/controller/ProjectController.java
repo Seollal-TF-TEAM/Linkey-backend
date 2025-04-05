5bb417c64cfcae4abcf31c1e9f161ae825927512
dyoun12 <gdaeyeon002@gmail.com>
2025-04-05 16:20:35 +0900

    refact : globalExceptionHandler를 이용한 CustomException 처리

--- spring-backend/src/main/java/com/linkey/core/controller/ProjectController.java
+++ spring-backend/src/main/java/com/linkey/core/controller/ProjectController.java
@@ -2,10 +2,8 @@ package com.linkey.core.controller;
 
 import com.linkey.core.domain.dto.request.ReqCreateProjectDto;
 import com.linkey.core.domain.dto.request.ReqUpdateProjectDto;
-import com.linkey.core.domain.dto.response.ResProjectDetailDto;
-import com.linkey.core.domain.dto.response.ResProjectListDto;
 import com.linkey.core.domain.dto.response.ResWrapper;
-import com.linkey.core.global.exception.CustomException;
+import com.linkey.core.domain.dto.response.SuccessResult;
 import com.linkey.core.service.project.ProjectServiceImpl;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -25,26 +23,16 @@ public class ProjectController {
     // http://localhost:8080/api/projects/projectList?githubUserId=103468518
     @GetMapping("projectList")
     @ResponseBody
-    public ResWrapper getProjectList(@RequestParam("githubUserId") long githubUserId) {
-        try {
-            ResProjectListDto resProjectListDto = projectService.getProjectsByGithubUserId(githubUserId);
-            return ResWrapper.resSuccess(resProjectListDto);
-        } catch (CustomException e){
-            return ResWrapper.resCustomException(e);
-        }
+    public ResWrapper<SuccessResult> getProjectList(@RequestParam("githubUserId") long githubUserId) {
+        return ResWrapper.resSuccess(projectService.getProjectsByGithubUserId(githubUserId));
     }
 
     // 테스트 완료
     // http://localhost:8080/api/projects/projectDetail?projectId=12
     @GetMapping("projectDetail")
     @ResponseBody
-    public ResWrapper getProjectDetail(@RequestParam("projectId") int projectId) {
-        try {
-            ResProjectDetailDto resProjectDetailDto = projectService.getProjectByProjectId(projectId);
-            return ResWrapper.resSuccess(resProjectDetailDto);
-        } catch (CustomException e) {
-            return ResWrapper.resCustomException(e);
-        }
+    public ResWrapper<SuccessResult> getProjectDetail(@RequestParam("projectId") int projectId) {
+        return ResWrapper.resSuccess(projectService.getProjectByProjectId(projectId));
     }
 
     // 테스트 완료
@@ -61,13 +49,8 @@ public class ProjectController {
      */
     @PostMapping("createProject")
     @ResponseBody
-    public ResWrapper createProject(@RequestBody ReqCreateProjectDto reqCreateProjectDto) {
-        try {
-            Integer projectId = projectService.createProject(reqCreateProjectDto);
-            return ResWrapper.resSuccess(projectId);
-        } catch (CustomException e){
-            return ResWrapper.resCustomException(e);
-        }
+    public ResWrapper<SuccessResult> createProject(@RequestBody ReqCreateProjectDto reqCreateProjectDto) {
+        return ResWrapper.resSuccess(projectService.createProject(reqCreateProjectDto));
     }
 
     // 테스트 완료
@@ -82,25 +65,15 @@ public class ProjectController {
      */
     @PatchMapping("updateProject")
     @ResponseBody
-    public ResWrapper updateProject(@RequestBody ReqUpdateProjectDto reqUpdateProjectDto) {
-        try {
-            Integer projectId = projectService.updateProject(reqUpdateProjectDto);
-            return ResWrapper.resSuccess(projectId);
-        } catch (CustomException e) {
-            return ResWrapper.resCustomException(e);
-        }
+    public ResWrapper<SuccessResult> updateProject(@RequestBody ReqUpdateProjectDto reqUpdateProjectDto) {
+        return ResWrapper.resSuccess(projectService.updateProject(reqUpdateProjectDto));
     }
 
     // 테스트 완료
     // http://localhost:8080/api/projects/deleteProject?projectId=1
     @DeleteMapping("deleteProject")
     @ResponseBody
-    public ResWrapper deleteProject(@RequestParam("projectId") int reqProjectId) {
-        try {
-            Integer projectId = projectService.deleteProject(reqProjectId);
-            return ResWrapper.resSuccess(projectId);
-        } catch (CustomException e) {
-            return ResWrapper.resCustomException(e);
-        }
+    public ResWrapper<SuccessResult> deleteProject(@RequestParam("projectId") int reqProjectId) {
+        return ResWrapper.resSuccess(projectService.deleteProject(reqProjectId));
     }
 }
